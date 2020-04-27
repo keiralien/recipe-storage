8ae2f96eff36a4dc7479b3966e2e5a4b1d80ef36
keiralien <jen.murphy8200@gmail.com>
2020-04-26 22:03:38 -0500

    search partially complete; update when Servings implemented

--- src/main/java/org/launchcode/recipestorage/models/Search.java
+++ src/main/java/org/launchcode/recipestorage/models/Search.java
@@ -5,12 +5,39 @@ import java.util.List;
 
 public class Search {
 
-    public static List<Recipe> find(Iterable<Recipe> allRecipes) {
+    /**
+     * General search method to locate a recipe.
+     * @param allRecipes a list of all recipes to be searched.
+     * @param searchTerm the value being searched.
+     * @param fieldType the field in which the value is beimg searched for.
+     * @return return a list of recipes that match the search criteria.
+     */
+    public static List<Recipe> find(Iterable<Recipe> allRecipes, String searchTerm, String fieldType) {
 
         List<Recipe> results = new ArrayList<>();
 
-        return (ArrayList<Recipe>) allRecipes;
+        if(searchTerm.toLowerCase().equals("all")) {
+            return (ArrayList<Recipe>) allRecipes;
+        }
+        if(fieldType.equals("all")) {
+            results = findByValue(allRecipes, searchTerm);
+            return results;
+        }
+        for (Recipe recipe : allRecipes) {
+            String fieldValue = fieldMatch(recipe, fieldType);
+            if (fieldValue != "" && fieldValue.toLowerCase().contains(searchTerm)) {
+                results.add(recipe);
+            }
+        }
+        return results;
     }
+
+    /**
+     * Search all recipes and all fields for the search term.
+     * @param allRecipes list of all recipes to search.
+     * @param value the searchTerm.
+     * @return a list of recipes where the searchTerm is in one of the fields.
+     */
 
     public static List<Recipe> findByValue(Iterable<Recipe> allRecipes, String value) {
 
@@ -30,5 +57,33 @@ public class Search {
             }
         }
         return results;
+    }
+
+    /**
+     * Search for value in the matching fieldTypes in Recipe class.
+     * @param recipe the recipe to search.
+     * @param fieldType the field being searched for.
+     * @return the value in the matching field.
+     */
+    public static String fieldMatch (Recipe recipe, String fieldType) {
+        String value;
+
+        if(fieldType.equals("name")) {
+            value = recipe.getName();
+        } else if (fieldType.equals("description")) {
+            value = recipe.getDescription();
+        } else if (fieldType.equals("category")) {
+            value = recipe.getCategories().toString();
+        } else if (fieldType.equals("ingredient")) {
+            value = recipe.getIngredients().toString();
+        } else if (fieldType.equals("directions")) {
+            value = recipe.getDirections().toString();
+        }
+//        else if (fieldType.equals("servings")) {
+//            value = recipe.getServings().toString();
+//        }
+        else {value = "";}
+
+        return value;
     }
 }
